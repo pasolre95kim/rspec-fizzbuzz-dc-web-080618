03b322b64b8343c7c1870cf5a04627a37772971f
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 21:39:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 class FizzBuzz
-  
+
 def fizzbuzz(n)
   if n % 3 == 0
     puts "Fizz"
@@ -9,7 +9,7 @@ def fizzbuzz(n)
 elsif n % 5 == 0
   puts "Buzz"
 
-elsif n % 3 == 0 && n % 5 == 0
+elsif (n % 3 == 0) && (n % 5 == 0)
   puts "FizzBuzz"
 
 else
