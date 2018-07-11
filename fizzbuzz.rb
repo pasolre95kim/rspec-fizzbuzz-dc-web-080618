0379aa27849483f4f25de9e58d7f1775f2022155
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 21:32:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,16 +3,16 @@
 
 def fizzbuzz (n)
   if n % 3 == 0
-    "Fizz"
+    puts "Fizz"
 
 elsif n % 5 == 0
-  "Buzz"
+  puts "Buzz"
 
 elsif n % 3 == 0 && n % 5 == 0
-  "FizzBuzz"
+  puts "FizzBuzz"
 
 else
-  "nil"
+  puts "nil"
 
   end
 end
