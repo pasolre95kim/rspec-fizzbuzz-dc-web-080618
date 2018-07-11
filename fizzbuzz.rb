02ea1c168e1a6c9a9c1596c4063fa7b4974b17c2
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 22:09:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,16 @@
 require 'pry'
 
 def fizzbuzz(n)
-  if n % 3 == 0
+
+if n % 3 == 0 && n % 5 == 0
+  puts "FizzBuzz"
+
+elsif n % 3 == 0
     return "Fizz"
 
  elsif n % 5 == 0
   return "Buzz"
 
-elsif n % 3 == 0 && n % 5 == 0
-  puts "FizzBuzz"
 
 else
   puts "nil"
