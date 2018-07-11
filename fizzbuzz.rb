9160cd7c0b83d9897a8fce3df841434a3fd033c3
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 21:22:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(n)
+def fizz_3(n)
   if n % 3 == 0 &&
     return "Fizz"
 
