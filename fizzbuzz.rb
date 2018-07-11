dac8ad120df82c9c385730ea47c11f5192b83f14
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 21:50:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+require 'pry'
 
+def fizzbuzz
+binding.pry
 end
 
 def fizzbuzz(n)
