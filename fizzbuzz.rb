7c86e51f123992af815142efd05a4c45584deded
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 21:54:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -21,3 +21,9 @@ else
 
   end
 end
+
+    fizz_3 = fizzbuzz(3)
+        fizz_5 = fizzbuzz(5)
+            fizz_15 = fizzbuzz(15)
+              fizz_4 = fizzbuzz(4)
+            expect(fizz_4).to eq(nil)
