a508906a059a5c86a450e64cbe22bcb7bb7a8dfc
Michelle Kim <pasolre95kim@gmail.com>
2018-07-11 21:55:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -14,16 +14,10 @@ elsif n % 5 == 0
   return "Buzz"
 
 elsif (n % 3 == 0) && (n % 5 == 0)
-  return "FizzBuzz"
+  returns "FizzBuzz"
 
 else
   puts "nil"
 
   end
 end
-
-    fizz_3 = fizzbuzz(3)
-        fizz_5 = fizzbuzz(5)
-            fizz_15 = fizzbuzz(15)
-              fizz_4 = fizzbuzz(4)
-            expect(fizz_4).to eq(nil)
